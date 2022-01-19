b8c9f788079ef627eb4ebc0fd1a6b53dd7f5eaf5
Abdwahid04 <kahftekto@gmail.com>
2022-01-19 15:49:52 +0700

    dikta

---
+++ README.md
@@ -0,0 +1,13 @@
+# **Android Sensor Engine**
+
+Made By : Dikta Fiana pertiwi 19.01.53.0127
+
+
+## Technologies in Use
+
+- Kotlin
+- Java
+- ViewBinding
+- Firebase
+- Pinned Shortcuts (For Android Home Screens)
+
---
+++ app/src/main/java/com/christianfoulcard/android/androidsensorengine/Constants.kt
@@ -0,0 +1,7 @@
+@file:JvmName("Constants")
+
+//Notification ID
+const val CHANNEL_ID = "123"
+const val VERBOSE_NOTIFICATION_CHANNEL_NAME = "Work Notification"
+const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION = "Work Notification Testing"
+
